f345f02b912e453289af0523c03b1b2a441a7a48
Pedram b.a <111277786+Pedminer20th@users.noreply.github.com>
2023-04-07 23:30:44 +0330

    Create debuger.cpp

---
+++ debuger.cpp
@@ -0,0 +1,20 @@
+// این تمرین جهت یاد اوری و اشنایی با سینتکس زبان سی پلاس پلاس میباشد
+// چندین خطا در کد زیر وجود دارد ان هارا اصلاح کرده
+
+#include <iostream>
+using namespace std ;
+
+int main()
+{
+    // Ask for your name and where live
+    
+    cout << "What is your name ? : " ;
+    cin >> name 
+
+    cout << "Where do you live > : " ;
+    cin >> location ;
+
+    // Say hello
+    cout << "Hello " << name <<  " from " << location << "." ;
+    return 0 ;
+}
